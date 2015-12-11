139d89b9a3f025874126ce374f6b2bc63a5de7d4
Seonghun Jo <jsh4051@gmail.com>
2015-12-11 12:29:44 +0900

    Add Unity Unit Test Tools

---
+++ JStreetViewer.CSharpTests/Assets/Scripts/StreetViewRendererTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class StreetViewRendererTests
+    {
+        [TestMethod()]
+        public void InstanceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StartRenderStreetViewTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
